1088f72ba32e4702c819a8ca265880241fe10f98
ern19 <aarontrammell19@gmail.com>
2017-10-26 21:25:16 -0400

    finished drinkView

--- lib/fridge_app.rb
+++ lib/fridge_app.rb
@@ -24,6 +24,13 @@ What you tryna do?
 1. List all fridges
 2. Add a fridge
 3. Delete a fridge
+4. View all food items from a specific fridge
+5. Add food to a fridge
+6. Eat some food from the fridge
+7. View all drinks in a fridge
+8. Add a drink to the fridge
+9. Consume part of a drink
+10. Consume all of a drink from the fridge
 )
 
 def deleteFridge
@@ -32,6 +39,44 @@ def deleteFridge
         delete = gets.chomp
         Fridge.find(delete).destroy
         puts "Fridge #{delete} has exploded"
+end
+
+def viewFoodItems
+    puts "Which fridge would you like to open?"
+    fridgeId = gets.chomp
+    foodInFridge = Food.where("fridge_id = '#{fridgeId}'")
+    foodInFridge.each do |food|
+        puts "You have #{food.name}"
+    end
+end
+
+def addSomeFood
+    puts "Which fridge?"
+    fridge_id = gets.chomp
+    puts "What is it?"
+    food_name = gets.chomp
+    puts "How much?"
+    food_weight = gets.chomp
+    Food.create(name: "#{food_name}", weight: "#{food_weight}", fridge_id: "#{fridge_id}")
+end
+
+def eatSomeFood
+    puts "Which fridge?"
+    fridge_id = gets.chomp
+    puts "Whatchu tryna eat?"
+    food_name = gets.chomp
+    eaten_food = Food.where("name = '#{food_name}'")
+    eaten_food.destroy_all
+    puts "Slow down man, you're gonna get a stomacheache."
+end
+
+def viewDrinks
+    puts "Which fridge?"
+    fridge_id = gets.chomp
+    drinks = Drink.all
+    drinks.each do |drink|
+        puts "Fridge #{fridge_id} has #{drink.name}"
+    end
 end
 puts my_str
 user_decision = gets.chomp
@@ -49,11 +94,18 @@ if user_decision == 3
     deleteFridge
 end
 
-my_str_2 = %q(
-    4. View all food items from a specific fridge
-5. Add food to a fridge
-6. Eat some food from the fridge
-7. View all drinks in a fridge
-8. Add a drink to the fridge
-9. Consume part of a drink
-10. Consume all of a drink from the fridge)
+if user_decision == 4 
+    viewFoodItems
+end
+
+if user_decision == 5
+    addSomeFood
+end
+
+if user_decision == 6
+    eatSomeFood
+end
+
+if user_decision == 7
+    viewDrinks
+end
